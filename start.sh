07a5ff7c1a2f66df43ce9e1456831a46a188bdbf
xzgowte <go@xzmail.orgs.hk>
2025-09-11 16:37:25 +0800

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,10 @@
+# 检查 cloudflared 是否存在，不存在则下载
+if [ ! -f ./cf ]; then
+    echo "cf 文件不存在，正在下载..."
+    wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64 -O cf
+    chmod +x ./cf
+fi
+
+# 启动 cloudflared 并写入日志
+npm start
+echo -e "启动成功"

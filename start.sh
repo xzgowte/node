25d13a0f8b672dd1e9736cf87efd7b15594cb07c
xzgowte <go@xzmail.orgs.hk>
2025-09-11 17:12:33 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,9 +2,8 @@
 if [ ! -f ./cf ]; then
     echo "cf 文件不存在，正在下载..."
     curl -L https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64 -O cf
-    chmod +x ./cf
 fi
-
+chmod +x ./cf
 # 启动 cloudflared 并写入日志
 npm start
 echo -e "启动成功"

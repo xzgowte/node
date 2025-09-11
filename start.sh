0a84674c4f80c249eab346ec2d15885947e17d60
xzgowte <go@xzmail.orgs.hk>
2025-09-11 16:43:23 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,7 @@
 # 检查 cloudflared 是否存在，不存在则下载
-apt update && apt install -y wget
 if [ ! -f ./cf ]; then
     echo "cf 文件不存在，正在下载..."
-    wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64 -O cf
+    curl -L https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64 -O cf
     chmod +x ./cf
 fi
 

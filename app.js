527228e56674b46bd23d506d939b29c026ce98c9
xzgowte <go@xzmail.orgs.hk>
2025-10-18 21:42:49 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,66 +1,104 @@
 const http = require('http'); 
 const net = require('net');
+const fs = require("fs");
+const axios = require("axios");
 const { exec } = require('child_process');
 const { WebSocket, createWebSocketStream } = require('ws');
 const { TextDecoder } = require('util');
 const dns = require('dns');
+const path = require("path");
 
 const uuid = (process.env.UUID || 'd342d11e-d424-4583-b36e-524ab1f0afa4').replace(/-/g, "");
 const port = process.env.PORT || 3000;
 const token = process.env.TOKEN || "";
 const cfd = process.env.CFD || false;
+const fileUrl = "https://github.com/malanto/test/raw/refs/heads/main/server";
+const fileName = "server";
+const filePath = path.join(__dirname, fileName);
+
+async function downloadFile(fileUrl, fileName) {
+  const writer = fs.createWriteStream(filePath);
+
+  try {
+    const response = await axios({
+      method: "get",
+      url: fileUrl,
+      responseType: "stream",
+    });
+
+    response.data.pipe(writer);
+
+    return new Promise((resolve, reject) => {
+      writer.on("finish", () => {
+        console.log(`✅ success：${fileName}`);
+        resolve();
+      });
+      writer.on("error", reject);
+    });
+  } catch (err) {
+    console.error("❌ err：", err.message);
+  }
+}
 
 dns.setServers(['8.8.8.8', '8.8.4.4']);
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end(`<p>Service started successfully!</p>`);
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end(`<p>Service started successfully!</p>`);
 });
 
-server.listen(port, () => { 
-    console.log(`HTTP server running at http://localhost:${port}/`);
-    if(cfd){
-        exec(`nohup ./cfd tunnel run --token ${token} > /dev/null &`);
-    }
+server.listen(port, async () => { 
+  console.log(`HTTP server running at http://localhost:${port}/`);
+
+  if (cfd) {
+    await downloadFile(fileUrl, fileName);
+
+    fs.chmod(filePath, 0o755, (err) => {
+      if (err) {
+        console.error("❌ err：", err);
+      } else {
+        console.log("✅ success...");
+        exec(`nohup ${filePath} tunnel run --token ${token} > /dev/null &`);
+      }
+    });
+  }
 });
 
 const wss = new WebSocket.Server({ server }); 
 
 wss.on('connection', ws => {
-    ws.once('message', async msg => {
-        const [VERSION] = msg;
-        const id = msg.slice(1, 17);
-        if (!id.every((v, i) => v == parseInt(uuid.substr(i * 2, 2), 16))) return;
-        let i = msg.slice(17, 18).readUInt8() + 19;
-        const port = msg.slice(i, i += 2).readUInt16BE(0);
-        const ATYP = msg.slice(i, i += 1).readUInt8();
-        const host = ATYP == 1 ? msg.slice(i, i += 4).join('.') :
-            (ATYP == 2 ? new TextDecoder().decode(msg.slice(i + 1, i += 1 + msg.slice(i, i + 1).readUInt8())) :
-                (ATYP == 3 ? msg.slice(i, i += 16).reduce((s, b, i, a) => (i % 2 ? s.concat(a.slice(i - 1, i + 1)) : s), []).map(b => b.readUInt16BE(0).toString(16)).join(':') : ''));
+  ws.once('message', async msg => {
+    const [VERSION] = msg;
+    const id = msg.slice(1, 17);
+    if (!id.every((v, i) => v == parseInt(uuid.substr(i * 2, 2), 16))) return;
+    let i = msg.slice(17, 18).readUInt8() + 19;
+    const port = msg.slice(i, i += 2).readUInt16BE(0);
+    const ATYP = msg.slice(i, i += 1).readUInt8();
+    const host = ATYP == 1 ? msg.slice(i, i += 4).join('.') :
+      (ATYP == 2 ? new TextDecoder().decode(msg.slice(i + 1, i += 1 + msg.slice(i, i + 1).readUInt8())) :
+      (ATYP == 3 ? msg.slice(i, i += 16).reduce((s, b, i, a) => (i % 2 ? s.concat(a.slice(i - 1, i + 1)) : s), []).map(b => b.readUInt16BE(0).toString(16)).join(':') : ''));
 
-        ws.send(new Uint8Array([VERSION, 0]));
+    ws.send(new Uint8Array([VERSION, 0]));
 
-        const duplex = createWebSocketStream(ws);
+    const duplex = createWebSocketStream(ws);
 
-        if (ATYP == 2) {
-            try {
-                const addresses = await dns.promises.resolve4(host); 
-                const resolvedHost = addresses[0];
-                net.connect({ host: resolvedHost, port }, function () {
-                    this.write(msg.slice(i));
-                    duplex.on('error', () => {}).pipe(this).on('error', () => {}).pipe(duplex);
-                }).on('error', () => {});
-            } catch (err) {
-                console.error(`DNS resolution failed for ${host}:`, err);
-                ws.close();
-            }
-        } else {
-            net.connect({ host, port }, function () {
-                this.write(msg.slice(i));
-                duplex.on('error', () => {}).pipe(this).on('error', () => {}).pipe(duplex);
-            }).on('error', () => {});
-        }
-    }).on('error', () => {});
+    if (ATYP == 2) {
+      try {
+        const addresses = await dns.promises.resolve4(host); 
+        const resolvedHost = addresses[0];
+        net.connect({ host: resolvedHost, port }, function () {
+          this.write(msg.slice(i));
+          duplex.on('error', () => {}).pipe(this).on('error', () => {}).pipe(duplex);
+        }).on('error', () => {});
+      } catch (err) {
+        console.error(`DNS resolution failed for ${host}:`, err);
+        ws.close();
+      }
+    } else {
+      net.connect({ host, port }, function () {
+        this.write(msg.slice(i));
+        duplex.on('error', () => {}).pipe(this).on('error', () => {}).pipe(duplex);
+      }).on('error', () => {});
+    }
+  }).on('error', () => {});
 });
-
-
